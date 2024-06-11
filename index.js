8011c4fd912fb4e0225aea19310392da57c8a542
Romario-Diaz <domsunday.22@gmail.com>
2024-06-10 21:10:21 -0500

    feat: 🎸 receive parameters
    receiving parameters with get method

--- index.js
+++ index.js
@@ -11,9 +11,32 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product One',
+      price: 1000,
+    },
+    {
+      name: 'Product Two',
+      price: 2000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
+    id,
     name: 'Product One',
     price: 1000,
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
   });
 });
 
