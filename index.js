619dd2ac905496cdcd6d8e83dc99b4797c99d196
Romario-Diaz <domsunday.22@gmail.com>
2024-06-10 20:46:47 -0500

    feat: 🎸 adding routes
    Adding new routes in the application
    
    BREAKING CHANGE: 🧨 main

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hola mundo');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola Soy un nuevo endpoint');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product One',
+    price: 1000,
+  });
+});
+
 app.listen(port, () => {
   console.log('Mi port', port);
 });
